46d488c65d341e9609988aa8bd044707a55764e8
Thilo Cestonaro <thilo@cestona.ro>
2017-03-31 07:49:50 +0200

    be able to specify the cmd at the commandline

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,29 @@
 #include <string>
 #include <iostream>
+#include <boost/algorithm/hex.hpp>
 
 #include "serial.h"
 
 using namespace std;
 
+vector<uint8_t> h2b(string s) {
+	vector<uint8_t> res;
+	boost::algorithm::unhex(s.begin(), s.end(), back_inserter(res));
+	return res;
+}
+
 int main(int argc, const char* argv[])
 {
 	Serial port("/dev/ttyACM0", 115200, 1, 0, 1);
+	string cmd = "0008";
 	vector<uint8_t> request { 0x00, 0x08 };
 	vector<uint8_t> response; // 0x00, 0x01, 0x80, 0x20, 0x04, 0x66, 0xCF, 0x4D, 0xC2
 
-	port.setCrc(false);
+	if(argc > 1)
+		cmd = argv[1];
+
+	request = h2b(cmd);
+	port.setCrc(true);
 	response = port.write_read(request);
 	return 0;
 }
